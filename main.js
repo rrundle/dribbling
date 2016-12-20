55411165b719519d0a463089af08a9d4c2bbd7fb
Ryan Rundle <ryan.rundle@ymail.com>
2016-12-20 13:32:27 -0800

    Added constructor function and prototype, #1.

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 //functions and prototypes
 
+function Player(location, direction, speed, marker) {
+  this.location = location
+  this.direction = direction
+  this.speed = speed
+  this.marker = marker
+  this.marker.setAttribute('id', 'player')
+}
+
+Player.prototype.NewSpot = function() {
+  if (this.direction === 'north') {
+    this.location[1] -= this.speed
+  }
+  if (this.direction === 'south') {
+    this.location[1] += this.speed
+  }
+  if (this.direction === 'west') {
+    this.location[0] -= this.speed
+  }
+  if (this.direction === 'east') {
+    this.location[0] += this.speed
+
+  }
+  if (this.direction === 'stop') {
+    this.speed = 0
+  }
+  this.marker.style.top = this.location[1] + 'px'
+  this.marker.style.left = this.location[0] + 'px'
+}
+
 function viewSwitch(hide, view) {
   for (var i = 0; i < hide.length; i++) {
     hide[i].style.visibility = 'hidden'
