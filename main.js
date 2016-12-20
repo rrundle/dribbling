1a356c154f79499a7afbaf0ec4ae795fbb922ade
Ryan Rundle <ryan.rundle@ymail.com>
2016-12-20 13:28:04 -0800

    Added event listener for view switch on game start, #1.

--- main.js
+++ main.js
@@ -1,65 +1,19 @@
-function Car(location, direction, speed, marker) {
-  this.location = location
-  this.direction = direction
-  this.speed = speed
-  this.marker = marker
-  this.marker.setAttribute('class', 'player')
+//functions and prototypes
+
+function viewSwitch(hide, view) {
+  for (var i = 0; i < hide.length; i++) {
+    hide[i].style.visibility = 'hidden'
+  }
+  for (var j = 0; j < view.length; j++) {
+    view[j].style.visibility = 'visible'
+  }
 }
 
-Car.prototype.NewSpot = function() {
-  if (this.direction === 'north') {
-    this.location[1] -= this.speed
-  }
-  if (this.direction === 'south') {
-    this.location[1] += this.speed
-  }
-  if (this.direction === 'west') {
-    this.location[0] -= this.speed
-  }
-  if (this.direction === 'east') {
-    this.location[0] += this.speed
-
-  }
-  if (this.direction === 'stop') {
-    this.speed = 0
-  }
-  this.marker.style.top = this.location[1] + 'px'
-  this.marker.style.left = this.location[0] + 'px'
-}
-
-var ride = document.querySelector('.player')
-
-var bugatti = new Car([0, 0], 'east', 1.5, ride)
-
-var button = document.getElementsByName('button')
+//event listeners
 document.addEventListener('click', function(e) {
-  if (e.target == button) {
-    setInterval(function() {
-    bugatti.NewSpot()
-    }, 1)
-  }
-})
-
-document.addEventListener('keydown', function(e) {
-  switch(e.keyCode) {
-    case 38:
-    bugatti.direction = 'north'
-    break
-
-    case 40:
-    bugatti.direction = 'south'
-    break
-
-    case 39:
-    bugatti.direction = 'east'
-    break
-
-    case 37:
-    bugatti.direction = 'west'
-    break
-
-    default:
-    bugatti.speed = 0
-    break
+  if (e.target.className.indexOf('intro') !== -1) {
+    var hide = document.querySelectorAll('.intro')
+    var view = document.querySelectorAll('.game')
+    viewSwitch(hide, view)
   }
 })
